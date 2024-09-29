63868c647d6fe3e91ec280fcb69fc8b769a196d2
Shobby789 <smshoaib2001@gmail.com>
2024-09-30 00:36:15 +0500

    added give butter url in donate button

--- components/Global/Button.jsx
+++ components/Global/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Button = () => {
+const Button = ({ onclick }) => {
   return (
-    <button className="relative overflow-hidden bg-primary text-white font-medium py-2 px-4 rounded-full w-40 h-12">
+    <button
+      type="button"
+      onClick={onclick}
+      className="relative overflow-hidden bg-primary text-white font-medium py-2 px-4 rounded-full w-40 h-12"
+    >
       <span className="absolute inset-0 flex items-center justify-center transition-transform duration-[0.4s] ease-in-out transform button-text1">
         Donate Us
       </span>
