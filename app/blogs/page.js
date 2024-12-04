ac23fc8d07db1de663fd5e90543c453a4acc9160
Shobby789 <smshoaib2001@gmail.com>
2024-12-04 08:02:45 +0500

    added blogs page

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,6 +1,11 @@
 import BlogsPage from "@/components/Blogs/BlogsPage";
 import React from "react";
 
+export const metadata = {
+  title: "Palmcare - Blogs",
+  description: "Palmcare",
+};
+
 const page = () => {
   return (
     <div>
